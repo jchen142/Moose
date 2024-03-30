d2c8b32a07f981e5e1c4a532977dd1f80314831b
Joshua Chen <60044423+jchen142@users.noreply.github.com>
2024-03-30 14:00:06 -0700

    Added music functionality
    Still need to iron out some bugs after incorporation of music functions.

--- src/mooseBot.py
+++ src/mooseBot.py
@@ -1,6 +1,7 @@
 import os
 import asyncio
 import discord
+import youtube_dl
 from src.log import logger
 
 from g4f.client import Client
@@ -13,6 +14,46 @@ from discord import app_commands
 from src import log, art, personas
 
 
+# Suppress noise about console usage from errors:
+youtube_dl.utils.bug_reports_message = lambda: ''
+ytdl_format_options = {
+    'format': 'bestaudio/best',
+    'outtmpl': '%(extractor)s-%(id)s-%(title)s.%(ext)s',
+    'restrictfilenames': True,
+    'noplaylist': True,
+    'nocheckcertificate': True,
+    'ignoreerrors': False,
+    'logtostderr': False,
+    'quiet': True,
+    'no_warnings': True,
+    'default_search': 'auto',
+    'source_address': '0.0.0.0',  # bind to ipv4 since ipv6 addresses cause issues sometimes
+}
+
+ffmpeg_options = {
+    'options': '-vn',
+}
+
+ytdl = youtube_dl.YoutubeDL(ytdl_format_options)
+
+class YTDLSource(discord.PCMVolumeTransformer):
+    def __init__(self, source, *, data, volume=0.5):
+        super().__init__(source, volume)
+        self.data = data
+        self.title = data.get('title')
+        self.url = data.get('url')
+
+    @classmethod
+    async def from_url(cls, url, *, loop=None, stream=False):
+        loop = loop or asyncio.get_event_loop()
+        data = await loop.run_in_executor(None, lambda: ytdl.extract_info(url, download=not stream))
+        if 'entries' in data:
+            # Take first item from a playlist:
+            data = data['entries'][0]
+        filename = data['url'] if stream else ytdl.prepare_filename(data)
+        return cls(discord.FFmpegPCMAudio(filename, **ffmpeg_options), data=data)
+
+
 def run_discord_bot():
     @discordClient.event
     async def on_ready():
@@ -21,6 +62,34 @@ def run_discord_bot():
         loop = asyncio.get_event_loop()
         loop.create_task(discordClient.process_messages())
         logger.info(f'{discordClient.user} is ready!')
+
+    @discordClient.tree.command(name="join", description="Joins current voice channel")
+    async def join(interaction: discord.Interaction):
+        voice_channel = interaction.user.voice.channel
+        if voice_channel:
+            await voice_channel.connect()
+        else:
+            await interaction.followup.send("You are not in a voice channel.")
+
+    @discordClient.tree.command(name="stream", description="Play YT URL")
+    async def stream(interaction: discord.Interaction, url: str):
+        """Streams a video from Youtube without predownloading"""
+        voice_client = interaction.guild.voice_client
+        if voice_client:
+            player = await YTDLSource.from_url(url, stream=True)
+            voice_client.play(player, after=lambda e: print(f"Player error: {e}") if e else None)
+            await interaction.followup.send(f"Now playing: {player.title}")
+        else:
+            await interaction.followup.send("I am not connected to a voice channel.")
+
+    @discordClient.tree.command(name="stop", description="Bot stops playing music and leaves")
+    async def stop(interaction: discord.Interaction):
+        """Stops the bot's music and disconnects"""
+        voice_client = interaction.guild.voice_client
+        if voice_client:
+            await voice_client.disconnect()
+        else:
+            await interaction.followup.send("I am not connected to a voice channel.")
 
     @discordClient.tree.command(name="chat", description="Have a chat with Moose")
     async def chat(interaction: discord.Interaction, *, message: str):
